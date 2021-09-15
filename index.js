34c017756278ad8827e0a3039898074858d48397
Rachel Kim <rachelkim8084@gmail.com>
2021-09-14 23:15:28 -0700

    added input form

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 "use strict"
+
+let displayEl = document.querySelector("#display");
+let leftBtnEl = document.querySelector("#left-btn");
+let rightBtnEl = document.querySelector("#right-btn");
+let timeEl = document.querySelector("#time");
+
+let studyTime, breakTime;
+
+leftBtnEl.addEventListener("click", function() {
+    if (leftBtnEl.classList.contains("ok")) {
+        // add code
+    }
+    if (leftBtnEl.classList.contains("reset")) {
+        // add code
+    }
+});
+
+rightBtnEl.addEventListener("click", function() {
+    if (rightBtnEl.classList.contains("clear")) {
+        timeEl.value = null;
+    }
+    if (rightBtnEl.classList.contains("stop")) {
+        // add code
+    }
+});
